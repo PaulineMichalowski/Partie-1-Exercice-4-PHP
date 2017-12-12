e06b9507e637979ac7b302a2c6b5810e260db52b
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-12 11:13:31 +0100

    Création de l'exercice 4

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$variable_string = 'Pauline';
+$variable_int = 12;
+$variable_float = 12.5;
+$variable_bool = true;
+echo $variable_string . ' ';
+echo $variable_int . ' ';
+echo $variable_float . ' ';
+echo $variable_bool;
+?>
